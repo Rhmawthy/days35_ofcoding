f58b4a4e40e91b75a79795e8199df26d45707a1f
Rahmawati <115470125+Rhmawthy@users.noreply.github.com>
2022-11-14 02:00:34 -0800

    Add files via upload

---
+++ days35_segitiga angka.py
@@ -0,0 +1,14 @@
+#program segitiga deret
+
+nilai = int(input("masukkan nilai:"))
+for number in range (1,nilai + 1):
+    print(" ".join([str(angka) for angka in range(1,number+1)]))
+    
+
+    
+    
+        
+        
+    
+        
+       
